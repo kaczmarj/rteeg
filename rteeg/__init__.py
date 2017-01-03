743ac7a7fd1a1f56ef85bf41fc9db782ab429d6e
jakubk <jakubk@mit.edu>
2017-01-03 18:29:33 -0500

    Added author info

--- rteeg/__init__.py
+++ rteeg/__init__.py
@@ -1,3 +1,4 @@
-"""Real-time streaming and analysis of EEG data with feedback"""
+"""Real-time streaming and analysis of EEG data."""
+# Author: Jakub Kaczmarzyk <jakubk@mit.edu>
 from .analysis import LoopAnalysis
 from .stream import EEGStream, MarkerStream
