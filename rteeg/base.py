08dd78e94a0560d608971b66fbe2da5bc335790a
jakubk <jakubk@mit.edu>
2017-01-03 10:47:02 -0500

    Repaired bugs

--- rteeg/base.py
+++ rteeg/base.py
@@ -13,10 +13,11 @@ def resolve_streams():
 
 class BaseStream(object):
 
-    def __init__(self, ):
+    def __init__(self):
         self._thread_lock = RLock()
         self._kill_signal = Event()
         self.data = []
+        self.connected = False
 
     def __del__(self):
         self._kill_signal.set()
@@ -47,11 +48,14 @@ class BaseStream(object):
         name : str
             Name for the thread.
         """
+        if self.connected:
+            raise RuntimeError("Stream already connected.")
         self._thread = Thread(target=target, name=name)
         self._thread.daemon = True
         self._thread.start()
+        self.connected = True
 
-    def copy_data(self, index=0):
+    def copy_data(self, index=None):
         """Copy `data` in a thread-safe manner.
 
         Parameters
@@ -59,9 +63,14 @@ class BaseStream(object):
         index : int
             Get last `index` items. By default, returns all items.
         """
-        current_max = len(self.data)
-        if index > current_max:
-            warnings.warn("Last {} samples were requested, but only {} are "
-                          "present.".format(index, current_max))
-        with self._thread_lock:
-            return [row[:] for row in self.data[-index:]]
+        if index is None:
+            with self._thread_lock:
+                return [row[:] for row in self.data]
+        else:
+            current_max = len(self.data)
+            if index > current_max:
+                warnings.warn("Last {} samples were requested, but only {} are "
+                              "present.".format(index, current_max))
+
+            with self._thread_lock:
+                return [row[:] for row in self.data[-index:]]
