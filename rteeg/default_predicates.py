74c0b4bf2ca56ae0b5ae6cc1fc3e8e91dd1c03e1
jakubk <jakubk@mit.edu>
2017-01-03 10:47:55 -0500

    Added default predicate for markers

--- rteeg/default_predicates.py
+++ rteeg/default_predicates.py
@@ -1,21 +1,19 @@
 """Dictionary of default LabStreamingLayer predicates for different EEG
-systems.
-
-These predicates can be changed to meet the user's requirements.
-
+systems and for Markers streams.
 
 The predicate string, e.g. "name='BioSemi'" or
 "type='EEG' and starts-with(name,'BioSemi') and
 count(description/desc/channels/channel)=32". For more info, refer to:
 http://en.wikipedia.org/w/index.php?title=XPath_1.0&oldid=474981951.
 
-
-Last updated 12/30/2016.
-
 Author: Jakub Kaczmarzyk, jakubk@mit.edu
 """
-
-default_predicates = {
+eeg = {
     'Enobio32': "type='EEG' and starts-with(desc/manufacturer,'NeuroElectrics')",
     'BioSemi32': "type='EEG'",
+}
+
+markers = {
+    'default': "type='Markers' and "
+               "not(starts-with(desc/manufacturer,'NeuroElectrics'))",
 }
