5312f59a133db8a984322a5b7ba2e8df15d5c89d
jakubk <jakubk@mit.edu>
2017-01-04 14:04:41 -0500

    Moved  out of EEGStream

--- rteeg/stream.py
+++ rteeg/stream.py
@@ -1,4 +1,5 @@
 # Author: Jakub Kaczmarzyk <jakubk@mit.edu>
+"""Classes to stream EEG data and event markers."""
 from __future__ import division
 
 import datetime
@@ -12,8 +13,8 @@ from mne.utils import ProgressBar
 import numpy as np
 from pylsl import StreamInlet, local_clock, resolve_bypred
 
+from rteeg import default_predicates
 from .base import BaseStream
-import default_predicates
 
 # How much MNE talks.
 set_log_level(verbose='error')
@@ -49,7 +50,7 @@ def _get_stream_inlet(lsl_predicate):
     stream = resolve_bypred(lsl_predicate)
     if len(stream) == 1:
         inlet = StreamInlet(stream[0])
-        print("Connected to stream.")
+        print("Connected to stream.")  # TODO: change this to logging.
     elif not stream:
         raise ValueError("Zero streams match the given predicate.")
     else:
@@ -57,6 +58,51 @@ def _get_stream_inlet(lsl_predicate):
                          "stream must match the predicate.")
     return inlet
 
+def make_events(data, marker_stream, event_duration=0):
+    """Create array of events.
+
+    This function creates an array of events that is compatible with
+    mne.Epochs. If no marker is found, returns ndarray indicating that
+    one event occurred at the same time as the first sample of the EEG data,
+    effectively making an Epochs object out of all of the data (until tmax
+    of mne.Epochs)
+
+    Parameters
+    ----------
+    data : array
+        EEG data in the shape (n_channels + timestamp, n_samples). Call
+        the method EEGStream._get_raw_eeg_data() to create this array.
+    marker_stream : rteeg.MarkerStream
+        Stream of marker data.
+    event_duration : int (defaults to 0)
+        Duration of each event marker in seconds. This is not epoch
+        duration.
+
+    Returns
+    -------
+    events : ndarray
+        Array of events in the shape (n_events, 3).
+    """
+    # Get the markers between two times.
+    lower_time_limit = data[-1, 0]
+    upper_time_limit = data[-1, -1]
+    with marker_stream._thread_lock:
+        tmp = np.array([row[:] for row in marker_stream.data
+                        if upper_time_limit >= row[-1] >= lower_time_limit],
+                       dtype=np.int32)
+    # Pre-allocate array for speed.
+    events = np.zeros(shape=(tmp.shape[0], 3), dtype=np.int32)
+    # If there is at least one marker ...
+    if tmp.shape[0] > 0:
+        for event_index, (marker_int, timestamp) in enumerate(tmp):
+            # Get the index where this marker happened in the EEG data.
+            eeg_index = (np.abs(data[-1, :] - timestamp)).argmin()
+            # Add a row to the events array.
+            events[event_index, :] = eeg_index, event_duration, marker_int
+    else:
+        # Make empty events array.
+        return np.array([[0, 0, 0]])
+    return events
 
 
 class EEGStream(BaseStream):
@@ -80,6 +126,11 @@ class EEGStream(BaseStream):
                                  "predicate defined in `default_predicates`. "
                                  "Without a valid predicate, streams cannot be "
                                  "found.".format(self.eeg_system))
+
+        self._stream_inlet = None
+        self._eeg_unit = None
+        self.info = None
+
         self.ica = ICA(method='extended-infomax')
         self.raw_for_ica = None
         # Search for and connect to a LabStreamingLayer stream of EEG data.
@@ -98,14 +149,14 @@ class EEGStream(BaseStream):
         # Get channel names.
         ch_names = []
         this_child = info.desc().child('channel')
-        for __ in range(info.channel_count()):
+        for _ in range(info.channel_count()):
             ch_names.append(this_child.child_value('name'))
             this_child = this_child.next_sibling('channel')
 
         # Get the EEG measurement unit (e.g., microvolts).
         units = []
         this_child = info.desc().child('channel')
-        for __ in range(info.channel_count()):
+        for _ in range(info.channel_count()):
             units.append(this_child.child_value('unit'))
             this_child = this_child.next_sibling('channel')
         if all(units):
@@ -124,8 +175,8 @@ class EEGStream(BaseStream):
                                 montage=self.eeg_system)
 
         # Add time of recording.
-        d = datetime.datetime.now()
-        timestamp = time.mktime(d.timetuple())
+        dt = datetime.datetime.now()
+        timestamp = time.mktime(dt.timetuple())
         self.info['meas_date'] = [timestamp, 0]
 
         # Record data in a while loop.
@@ -170,16 +221,16 @@ class EEGStream(BaseStream):
         if data_duration is None:
             data = np.array(self.copy_data()).T
             # Scale the data but not the timestamps.
-            data[:-1,:] = np.multiply(data[:-1,:], scale)
+            data[:-1, :] = np.multiply(data[:-1, :], scale)
         else:
             index = int(data_duration * self.info['sfreq'])
             data = np.array(self.copy_data(index)).T
             # Scale the data but not the timestamps.
-            data[:-1,:] = np.multiply(data[:-1,:], scale)
+            data[:-1, :] = np.multiply(data[:-1, :], scale)
         return data
 
     def make_raw(self, data_duration=None, apply_ica=True, first_samp=0,
-                 verbose=None, marker_stream=None):
+                 marker_stream=None, verbose=None):
         """Create instance of mne.io.RawArray.
 
         Parameters
@@ -201,65 +252,19 @@ class EEGStream(BaseStream):
         raw_data = self.get_data(data_duration=data_duration)
         # Add events if Markers stream was started.
         if marker_stream is None:
-            raw_data[-1,:] = 0  # Make row of timestamps a row of events 0.
+            raw_data[-1, :] = 0  # Make row of timestamps a row of events 0.
             raw = io.RawArray(raw_data, self.info, first_samp=first_samp,
                               verbose=verbose)
         else:
             raw = io.RawArray(raw_data, self.info, first_samp=first_samp,
                               verbose=verbose)
-            events = self.make_events(raw_data, marker_stream)
-            raw_data[-1,:] = 0  # Replace timestamps with zeros.
+            events = make_events(raw_data, marker_stream)
+            raw_data[-1, :] = 0  # Replace timestamps with zeros.
             raw.add_events(events)
         # If user wants to apply ICA and if ICA has been fitted ...
         if apply_ica and self.ica.current_fit != 'unfitted':
             return self.ica.apply(raw)
         return raw
-
-    def make_events(self, data, marker_stream, event_duration=0):
-        """Create array of events.
-
-        This function creates an array of events that is compatible with
-        mne.Epochs. If no marker is found, returns ndarray indicating that
-        one event occurred at the same time as the first sample of the EEG data,
-        effectively making an Epochs object out of all of the data (until tmax
-        of mne.Epochs)
-
-        Parameters
-        ----------
-        data : array
-            EEG data in the shape (n_channels + timestamp, n_samples). Call
-            the method _get_raw_eeg_data() to create this array.
-        marker_stream : rteeg.MarkerStream
-            Stream of marker data.
-        event_duration : int (defaults to 0)
-            Duration of each event marker in seconds. This is not epoch
-            duration.
-
-        Returns
-        -------
-        events : ndarray
-            Array of events in the shape (n_events, 3).
-        """
-        # Get the markers between two times.
-        lower_time_limit = data[-1,0]
-        upper_time_limit = data[-1,-1]
-        with marker_stream._thread_lock:
-            tmp = np.array([row[:] for row in marker_stream.data
-                             if upper_time_limit >= row[-1] >= lower_time_limit],
-                             dtype=np.int32)
-        # Pre-allocate array for speed.
-        events = np.zeros(shape=(tmp.shape[0], 3), dtype=np.int32)
-        # If there is at least one marker ...
-        if tmp.shape[0] > 0:
-            for event_index, (marker_int, timestamp) in enumerate(tmp):
-                # Get the index where this marker happened in the EEG data.
-                eeg_index = (np.abs(data[-1,:] - timestamp)).argmin()
-                # Add a row to the events array.
-                events[event_index, :] = eeg_index, event_duration, marker_int
-        else:
-            # Make empty events array.
-            return np.array([[0, 0, 0]])
-        return events
 
     def make_epochs(self, marker_stream, data_duration=None, events=None,
                     event_duration=0, event_id=None, apply_ica=True, tmin=-0.2,
@@ -293,9 +298,9 @@ class EEGStream(BaseStream):
         raw_data = self.get_data(data_duration=data_duration)
 
         if events is None:
-            events = self.make_events(raw_data, marker_stream, event_duration)
+            events = make_events(raw_data, marker_stream, event_duration)
 
-        raw_data[-1,:] = 0
+        raw_data[-1, :] = 0
         raw = io.RawArray(raw_data, self.info)
 
         # If user wants to apply ICA and if ICA has been fitted ...
@@ -303,12 +308,13 @@ class EEGStream(BaseStream):
             raw = self.ica.apply(raw)
 
         return Epochs(raw, events, event_id=event_id, tmin=tmin, tmax=tmax,
-                      baseline=baseline, picks=picks,name=name, preload=preload,
-                      reject=reject, flat=flat, proj=proj, decim=decim,
-                      reject_tmin=reject_tmin, reject_tmax=reject_tmax,
-                      detrend=detrend, add_eeg_ref=add_eeg_ref,
-                      on_missing=on_missing,
-                      reject_by_annotation=reject_by_annotation, verbose=verbose)
+                      baseline=baseline, picks=picks, name=name,
+                      preload=preload, reject=reject, flat=flat, proj=proj,
+                      decim=decim, reject_tmin=reject_tmin,
+                      reject_tmax=reject_tmax, detrend=detrend,
+                      add_eeg_ref=add_eeg_ref, on_missing=on_missing,
+                      reject_by_annotation=reject_by_annotation,
+                      verbose=verbose)
 
     def fit_ica(self, data, when='next', warm_start=False):
         """Conduct Independent Components Analysis (ICA) on a segment of data.
@@ -348,7 +354,7 @@ class EEGStream(BaseStream):
         if self.ica.current_fit != 'unfitted':
             self.ica = ICA(method='extended-infomax')
 
-        if type(data) is io.RawArray:
+        if isinstance(data, io.RawArray):
             self.raw_for_ica = data
 
         elif isinstance(data, numbers.Number):
@@ -377,12 +383,12 @@ class EEGStream(BaseStream):
                 print("")
 
             with self._thread_lock:
-                 _data = np.array([r[:] for r in
-                                   self.data[start_index:end_index]]).T
+                _data = np.array([r[:] for r in
+                                  self.data[start_index:end_index]]).T
 
             # Now we have the data array in _data. Use it to make instance of
             # mne.RawArray, and then we can compute the ICA on that instance.
-            _data[-1,:] = 0
+            _data[-1, :] = 0
 
             # Use previous data in addition to the specified data when fitting
             # the ICA, if the user requested this.
@@ -393,9 +399,9 @@ class EEGStream(BaseStream):
                 self.raw_for_ica = io.RawArray(_data, self.info)
 
         print("Computing ICA solution ...")
-        t0 = local_clock()
+        t_0 = local_clock()
         self.ica.fit(self.raw_for_ica.copy())  # Fits in-place.
-        print("Finished in {:.2f} s".format(local_clock() - t0))
+        print("Finished in {:.2f} s".format(local_clock() - t_0))
 
 
     def viz_ica(self, plot='components'):
@@ -447,6 +453,7 @@ class MarkerStream(BaseStream):
         super(MarkerStream, self).__init__()
         self.lsl_predicate_key = lsl_predicate_key
         self.lsl_predicate = default_predicates.markers[self.lsl_predicate_key]
+        self._stream_inlet = None
         self.connect(self._connect, 'Marker-data')
 
     def _connect(self):
