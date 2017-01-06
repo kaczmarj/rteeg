6854376622bf9cfb7292606b90c58d3f0eacbb6e
jakubk <jakubk@mit.edu>
2017-01-05 21:53:23 -0500

    Initialized streams inside function and added cleanup to end of function

--- rteeg/tests/test_stream.py
+++ rteeg/tests/test_stream.py
@@ -1,20 +1,21 @@
 """Tests for rteeg.stream.py"""
+import numpy as np
 from rteeg.stream import EEGStream, make_events, MarkerStream, _get_stream_inlet
 from rteeg.tests.utils import SyntheticData, true_markers
 
-eeg_outlet = SyntheticData("EEG", 32, 100)
-eeg = rteeg.EEGStream(eeg_system='test', lsl_predicate="type='EEG'")
-eeg.data = eeg_outlet.create_data(5000)
-
-marker_outlet = SyntheticData("Markers", 1, 1)
-markers = rteeg.MarkerStream()
-markers.data = marker_outlet.create_data(10)
-
 def test_stream():
+    # Create EEG and Marker streams / data.
+    eeg_outlet = SyntheticData("EEG", 32, 100)
+    eeg = EEGStream(eeg_system='Enobio32', lsl_predicate="type='EEG'")
+    eeg.data = eeg_outlet.create_data(5000)
+
+    marker_outlet = SyntheticData("Markers", 1, 1)
+    markers = MarkerStream()
+    markers.data = marker_outlet.create_data(10)
+
     # Test stream.make_events().
-    test_markers = rteeg.stream.make_events(eeg.get_data(), markers)
+    test_markers = make_events(eeg.get_data(), markers)
     assert np.array_equal(true_markers, test_markers), "Markers not created properly."
 
-
-# Clean up.
-eeg_outlet.stop()
+    # Clean up.
+    eeg_outlet.stop()
