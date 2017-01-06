5cfaa5c861efa44f450ce451bccf3aa9c7c5bd1d
jakubk <jakubk@mit.edu>
2017-01-06 14:44:06 -0500

    Destroyed thread at end of test_BaseStream_connect

--- rteeg/tests/test_base.py
+++ rteeg/tests/test_base.py
@@ -1,4 +1,5 @@
 """Tests for rteeg.base.py"""
+from __future__ import division
 import threading
 import time
 
@@ -13,22 +14,27 @@ from rteeg.tests.utils import SyntheticData
 def test_BaseStream_record_data_indefinitely():
     # Start a LabStreamingLayer stream of synthetic data.
     eeg_out = SyntheticData("EEG", 32, 100, send_data=True)
-    inlet = StreamInlet(resolve_streams()[0])
+    inlet = StreamInlet(resolve_streams(wait_time=1.)[0])
     base = BaseStream()
+
     # Check that length of base.data increases.
     len_0 = len(base.data)
     t = threading.Thread(target=base._record_data_indefinitely, args=(inlet,))
+    t.daemon = True
     t.start()
-    time.sleep(5.)
+    time.sleep(2.)
     len_1 = len(base.data)
     assert len_1 > len_0, "Data not being recorded."
+
     # Clean up.
+    base._kill_signal.set()
     eeg_out.stop()
 
 def test_BaseStream_connect():
+    event = threading.Event()
     def dummy_func():
-        while True:
-            time.sleep(5.)
+        while not event.is_set():
+            time.sleep(1.)
     base = BaseStream()
     n_threads_0 = threading.active_count()
     base.connect(dummy_func, "TEST")
@@ -44,15 +50,20 @@ def test_BaseStream_connect():
     with pytest.raises(RuntimeError):
         base.connect(dummy_func, "SECOND_TEST")
 
+    # Clean up.
+    event.set()
+
 def test_BaseStream_copy_data():
     # Start a LabStreamingLayer stream but do not stream data.
     eeg_outlet = SyntheticData("EEG", 32, 100, send_data=False)
     base = BaseStream()
+
     # Create the data.
     base.data = eeg_outlet.create_data(5000)
     assert base.copy_data() is not base.data, "Copy of data not deep enough."
     copy_equal = np.array_equal(np.array(base.copy_data()), np.array(base.data))
     assert copy_equal, "The copy is not equivalent to the original."
     assert len(base.copy_data(index=100)) == 100, "Indexing failed."
+
     # Clean up.
     eeg_outlet.stop()
