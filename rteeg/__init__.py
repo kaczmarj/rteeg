d2f71d39bb81de21c6a91265266e65b234ab6a94
jakubk <jakubk@mit.edu>
2017-01-06 15:06:13 -0500

    Removed .analysis temporarily

--- rteeg/__init__.py
+++ rteeg/__init__.py
@@ -1,4 +1,4 @@
 """Real-time streaming and analysis of EEG data."""
 # Author: Jakub Kaczmarzyk <jakubk@mit.edu>
-from .analysis import LoopAnalysis
+# from .analysis import LoopAnalysis
 from .stream import EEGStream, MarkerStream
