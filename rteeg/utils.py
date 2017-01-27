20a4509af87de57e613776c18f0686e23052ac98
jakubk <jakubk@mit.edu>
2017-01-27 17:10:26 -0500

    Allowed logger to print to console

--- rteeg/utils.py
+++ rteeg/utils.py
@@ -2,4 +2,24 @@
 # Author: Jakub Kaczmarzyk <jakubk@mit.edu>
 import logging
 
-logger = logging.getLogger(rteeg)
+
+def create_logger():
+    # Create logger.
+    logger = logging.getLogger('rteeg')
+    logger.setLevel(logging.DEBUG)
+    # Create console handler.
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # Create formatter and add to ch.
+    formatter = logging.Formatter('%(message)s')
+    ch.setFormatter(formatter)
+    # Add ch to logger.
+    logger.addHandler(ch)
+    return logger
+
+
+def set_log_level(verbosity):
+    logger.setLevel(verbosity)
+
+
+logger = create_logger()
